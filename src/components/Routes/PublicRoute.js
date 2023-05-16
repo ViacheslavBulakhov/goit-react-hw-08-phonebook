a66b15510cde8d8f136a68b4afb40f2f9fe9de8c
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2023-05-16 22:42:12 +0300

    fix selector

--- src/components/Routes/PublicRoute.js
+++ src/components/Routes/PublicRoute.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import authSelectors from 'redux/auth/auth-selectors';
 
 export default function PublicRoute({ children }) {
-  const isLoggedIn = useSelector(authSelectors.selectToken);
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
   const navigate = useNavigate();
 
   useEffect(() => {
